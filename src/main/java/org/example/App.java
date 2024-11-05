b4b6111e50b67745df0330611dea9c960789c3f3
Nabil Boudlal <nabil.boudlal@studenti.unicam.it>
2024-11-05 10:20:47 +0100

    first commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       JsonCreator jsonCreator = new JsonCreator();
+       jsonCreator.jsonCreateFile();
+    }
+}
---
+++ src/main/java/org/example/JsonCreator.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.json.simple.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class JsonCreator {
+
+public void jsonCreateFile() {
+    JSONObject jsonObject = new JSONObject();
+
+    jsonObject.put("Name", "Nabil");
+    jsonObject.put("Age", "23");
+    jsonObject.put("City", "Fermo");
+
+    try {
+        FileWriter file = new FileWriter("C:\\Users\\nabil\\OneDrive\\Desktop\\jsonExample.json");
+        file.write(jsonObject.toJSONString());
+        file.close();
+    } catch (IOException e) {
+        // TODO Auto-generated catch block
+        e.printStackTrace();
+    }
+    System.out.println("JSON file created: "+jsonObject);
+
+
+}
+
+}
